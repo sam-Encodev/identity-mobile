694633fda5d1ac457e8b87e85ed08ed482663b00
Sam <samuel@encodevlabs.com>
2025-03-24 17:37:56 +0000

    feat: update setup function to be asynchronous for improved initialization

--- lib/services/getit.dart
+++ lib/services/getit.dart
@@ -12,7 +12,7 @@ class Themer {
 
 final getIt = GetIt.instance;
 
-void setup() {
+void setup() async {
   getIt.registerSingleton<SaveContact>(SaveContact());
   getIt.registerSingleton<Themer>(Themer());
 }
