93b721f1dcc6d0452a45baddd8bd10772ca506ea
Sam <samuel@encodevlabs.com>
2025-03-24 17:36:51 +0000

    feat: add inputBorderRadius constant and update theme styles for consistency

--- lib/constants/styles.dart
+++ lib/constants/styles.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 const iconsSize = 38.0;
 const buttonRadius = 15.0;
+const inputBorderRadius = 30.0;
 const pagePadding = EdgeInsets.fromLTRB(15, 0, 15, 15);
 const icons = [
   Icon(Icons.phone_android_outlined, size: iconsSize),
