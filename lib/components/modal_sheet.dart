17ac8fc470b92915d65bf59aa07adf5a25f505ea
Sam <samuel@encodevlabs.com>
2025-03-24 19:28:06 +0000

    feat: enhance bottom sheet functionality with contact auto-save prompt and improved dialog styling

--- lib/components/modal_sheet.dart
+++ lib/components/modal_sheet.dart
@@ -2,14 +2,17 @@ import 'package:forui/forui.dart';
 import 'package:flutter/material.dart';
 import 'package:identity/constants/text.dart';
 import 'package:identity/components/snack_bar.dart';
+import 'package:identity/services/shared_pref.dart';
 import 'package:identity/services/url_launcher.dart';
 
-void bottomsheet(BuildContext context, data) {
+void bottomsheet(BuildContext context, data, {required bool dbCall}) {
   showModalBottomSheet<void>(
     context: context,
     builder: (_) {
       var name = data.name;
       var mobile = data.mobile;
+      var pref = SharedPref.get();
+      // print({"pref": pref});
 
       return SizedBox(
         height: 320,
@@ -29,7 +32,91 @@ void bottomsheet(BuildContext context, data) {
                   },
                 ),
               ),
-              ListTile(title: Text(name)),
+              if (dbCall == false && pref == false)
+                ListTile(
+                  onTap: () {},
+                  contentPadding: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                  minVerticalPadding: 0,
+                  trailing: IconButton(
+                    icon: const Icon(Icons.info_outline_rounded),
+                    onPressed: () {
+                      showAdaptiveDialog<void>(
+                        context: context,
+                        builder:
+                            (context) => FDialog(
+                              style: FDialogStyle(
+                                horizontalStyle: FDialogContentStyle(
+                                  titleTextStyle: TextStyle(),
+                                  bodyTextStyle: TextStyle(
+                                    color:
+                                        Theme.of(context).colorScheme.outline,
+                                  ),
+                                  padding: EdgeInsets.all(20.0),
+                                  actionPadding: double.minPositive,
+                                ),
+
+                                verticalStyle: FDialogContentStyle(
+                                  titleTextStyle: TextStyle(),
+                                  bodyTextStyle: TextStyle(
+                                    color:
+                                        Theme.of(context).colorScheme.outline,
+                                  ),
+                                  padding: EdgeInsets.all(20.0),
+                                  actionPadding: double.minPositive,
+                                ),
+                                decoration: BoxDecoration(
+                                  color:
+                                      Theme.of(
+                                        context,
+                                      ).colorScheme.onInverseSurface,
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
+                              ),
+                              direction: Axis.horizontal,
+                              body: Padding(
+                                padding: const EdgeInsets.only(bottom: 10.0),
+                                child: RichText(
+                                  text: TextSpan(
+                                    text: 'Contact is not saved. ',
+                                    style: TextStyle(
+                                      color:
+                                          Theme.of(context).colorScheme.outline,
+                                    ),
+                                    children: const <TextSpan>[
+                                      TextSpan(text: 'Go to '),
+                                      TextSpan(
+                                        text: 'Settings ',
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      TextSpan(
+                                        text:
+                                            'to enable contact auto-save feature.',
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                              actions: [
+                                FilledButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text(close),
+                                ),
+                              ],
+                            ),
+                      );
+                    },
+                  ),
+                ),
+              ListTile(
+                title: Text(
+                  name,
+                  style: TextStyle(overflow: TextOverflow.ellipsis),
+                ),
+              ),
               ListTile(
                 leading: FIcon(FAssets.icons.messageSquare, color: Colors.grey),
                 title: const Text(text),
